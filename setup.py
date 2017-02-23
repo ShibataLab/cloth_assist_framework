2b45630abf2318105bcab6975cecf0332f0192ef
Nishanth Koganti <buntyke@gmail.com>
2017-02-23 17:36:14 +0900

    [minor] update config files

--- setup.py
+++ setup.py
@@ -3,6 +3,5 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-d = generate_distutils_setup()
-
+d = generate_distutils_setup(packages=['cloth_assist'], package_dir={'': 'src'})
 setup(**d)
