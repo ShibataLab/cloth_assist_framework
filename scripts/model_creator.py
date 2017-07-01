7ec95030d3d6125c303e39d8b411eeed862628d3
Koganti Nishanth <buntyke@gmail.com>
2017-07-01 16:43:14 +0900

    [major] additional arguments for model training code

--- scripts/model_creator.py
+++ scripts/model_creator.py
@@ -21,8 +21,10 @@ def plotScales(scales, options, yThresh=0.05):
     ax = fig.add_subplot(111)
 
     x = np.arange(1,scales.shape[0]+1)
-    ax.bar(x, height=scales, width=0.8, align='center', color='b', edgecolor='k', linewidth=1.3)
-    ax.plot([0.4, scales.shape[0]+0.6], [yThresh, yThresh], '--', linewidth=3, color='r')
+    ax.bar(x, height=scales, width=0.8, align='center', color='b', edgecolor='k',
+           linewidth=1.3)
+    ax.plot([0.4, scales.shape[0]+0.6], [yThresh, yThresh], '--', linewidth=3,
+            color='r')
 
     # setting the bar plot parameters
     ax.set_xlim(.4, scales.shape[0]+.6)
@@ -34,10 +36,11 @@ def plotScales(scales, options, yThresh=0.05):
 
     return ax
 
-def plotLatent(inX, title, model=None, plotIndices=None, plot_inducing=False, plot_variance=False, max_points=[800,300]):
+def plotLatent(inX, title, model=None, plotIndices=None):
     s = 100
     marker = 'o'
     resolution = 50
+    qDim = model.X.mean.shape[1]
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -48,16 +51,6 @@ def plotLatent(inX, title, model=None, plotIndices=None, plot_inducing=False, pl
         print plotIndices
     input1, input2 = plotIndices
 
-    if inX[0].shape[0] > max_points[0]:
-        print("Warning".format(inX[0].shape))
-        subsample = np.random.choice(inX[0].shape[0], size=max_points[0], replace=False)
-        inX[0] = inX[0][subsample]
-
-    if inX[1].shape[0] > max_points[1]:
-        print("Warning".format(inX[1].shape))
-        subsample = np.random.choice(inX[1].shape[0], size=max_points[1], replace=False)
-        inX[1] = inX[1][subsample]
-
     xmin, ymin = inX[0][:, [input1, input2]].min(0)
     xmax, ymax = inX[0][:, [input1, input2]].max(0)
     x_r, y_r = xmax-xmin, ymax-ymin
@@ -65,33 +58,32 @@ def plotLatent(inX, title, model=None, plotIndices=None, plot_inducing=False, pl
     xmax += .1*x_r
     ymin -= .1*y_r
     ymax += .1*y_r
-    print xmin, xmax, ymin, ymax
 
-    if plot_variance:
-        def plot_function(x):
-            Xtest_full = np.zeros((x.shape[0], X.shape[1]))
-            Xtest_full[:, [input1, input2]] = x
-            _, var = model.predict(np.atleast_2d(Xtest_full))
-            var = var[:, :1]
-            return np.log(var)
+    def plotFunction(x):
+        Xtest_full = np.zeros((x.shape[0], qDim))
+        Xtest_full[:, [input1, input2]] = x
+        _, var = model.predict(np.atleast_2d(Xtest_full))
+        var = var[:, :1]
+        return -np.log(var)
 
-        X = inX[0]
-        view = ImshowController(ax,plot_function,(xmin,ymin,xmax,ymax),resolution,aspect='auto',interpolation='bilinear',cmap=cm.binary)
+    x, y = np.mgrid[xmin:xmax:1j*resolution, ymin:ymax:1j*resolution]
+    gridData = np.hstack((x.flatten()[:, None], y.flatten()[:, None]))
+    gridVariance = (plotFunction(gridData)).reshape((resolution, resolution))
 
-    trainH = ax.scatter(inX[0][:, input1], inX[0][:, input2], marker=marker, s=s, c='b', linewidth=.2, edgecolor='k', alpha=1.)
-    testH = ax.scatter(inX[1][:, input1], inX[1][:, input2], marker=marker, s=s, c='r', linewidth=.2, edgecolor='k', alpha=0.9)
+    varianceHandle = plt.imshow(gridVariance.T, interpolation='bilinear',
+                                origin='lower', cmap=cm.gray, extent=(xmin, xmax, ymin, ymax))
+
+    trainH = ax.scatter(inX[0][:, input1], inX[0][:, input2], marker=marker, s=s,
+                        c='b', linewidth=.2, edgecolor='k', alpha=1.)
+    testH = ax.scatter(inX[1][:, input1], inX[1][:, input2], marker=marker, s=s,
+                       c='r', linewidth=.2, edgecolor='k', alpha=0.9)
 
     ax.grid(b=False)
     ax.set_aspect('auto')
     ax.set_title(title, fontsize=25)
     ax.tick_params(axis='both', labelsize=20)
-    #ax.legend([trainH,testH],['Train','Test'],loc=1,fontsize=20)
     ax.set_xlabel('Latent Dimension %i' % (input1+1), fontsize=20)
     ax.set_ylabel('Latent Dimension %i' % (input2+1), fontsize=20)
-
-    if plot_inducing:
-        Z = model.Z
-        ax.scatter(Z[:, input1], Z[:, input2], c='w', s=25, marker="^", edgecolor='k', linewidth=.3, alpha=.6)
 
     ax.set_xlim((xmin, xmax))
     ax.set_ylim((ymin, ymax))
@@ -111,10 +103,23 @@ def main():
                         help='Enter filename to save trained model')
     parser.add_argument('--init', '-i', type=str, default='pca',
                         help='Enter initialization method for BGPLVM')
+    parser.add_argument('--sampleRate', '-r', type=int, default=1,
+                        help='Sampling rate of training dataset')
+    parser.add_argument('--numIters', '-n', type=int, default=100,
+                        help='iterations for training model')
+    parser.add_argument('--numInd', '-q', type=int, default=100,
+                        help='number of inducing points for approximation')
+    parser.add_argument('--numPlot', '-p', type=int, default=100,
+                        help='number of points for latent space plot')
+
     args = parser.parse_args()
 
     # parsing the arguments
     init = args.init
+    nInd = args.numInd
+    nPlot = args.numPlot
+    nIters = args.numIters
+    sRate = args.sampleRate
     inputVar = args.fileName
     outputVar = args.saveName
 
@@ -124,17 +129,17 @@ def main():
     data = data.view(np.float).reshape(data.shape + (-1,))
 
     # create training data
-    trainData = data[:,1:]
+    trainData = data[::sRate,1:]
 
     # model name
     expName = 'cloth_assist_model'
 
     # set the overall parameters for bgplvm
     qDim = 5
-    nSamples = data.shape[0]
+    nSamples = trainData.shape[0]
 
     # set the number of inducing inputs
-    nInducing = 50
+    nInducing = nInd
 
     # get latent points and scales
 
@@ -146,7 +151,7 @@ def main():
         scales = pca.explained_variance_ratio_
         scales = scales/scales.max()
     elif init == 'rand':
-        xData = np.random.normal(0, 1, (data.shape[0],qDim))
+        xData = np.random.normal(0, 1, (trainData.shape[0],qDim))
         scales = np.ones(qDim)
 
     # setting up the kernel
@@ -165,8 +170,8 @@ def main():
     bgplvmModel.rbf.variance.fix(var)
     bgplvmModel.Gaussian_noise.variance.fix(var/SNR)
 
-    initVardistIters = 50
-    bgplvmModel.optimize(messages = True, max_iters = initVardistIters)
+    initIters = nIters
+    bgplvmModel.optimize(messages = True, max_iters = initIters)
 
     # training without constraints
     trainIters = 500
@@ -174,15 +179,14 @@ def main():
     bgplvmModel.optimize(messages = True, max_iters = trainIters)
 
     # plot results
-    bgplvmX = [bgplvmModel.X.mean, np.zeros((1,qDim))]
+    bgplvmX = [bgplvmModel.X.mean[:nPlot,:], np.zeros((1,qDim))]
 
     scalesBGPLVM = bgplvmModel.kern.input_sensitivity(summarize=False)
     scalesBGPLVM =  scalesBGPLVM/scalesBGPLVM.max()
 
     options = {'title':'Bayesian GPLVM','ylabel':'ARD Weight'}
     plotScales(scalesBGPLVM,options)
-    plotLatent(bgplvmX, 'Bayesian GPLVM', model=bgplvmModel,
-               plot_variance=False, max_points=[1800,400])
+    plotLatent(bgplvmX, 'Bayesian GPLVM', model=bgplvmModel)
     plt.show()
 
     # save the model to file
